b676fdd3b2bfae1461b2c6643fa98015f8a54f7d
alexblacknn <alexblacknn@yandex.ru>
2023-11-13 16:42:36 +0300

    add json check and totol time response

--- HT/locust/src/assertion.py
+++ HT/locust/src/assertion.py
@@ -4,3 +4,13 @@ import http
 def check_http_response(transaction, response):
     if response.status_code != 200:
         response.failure(f'failed with status code {response.status_code}')
+    elif response.json()[0]["name"] != "Moscow":
+        response.failure(
+            f"Get unexpected value in 'name' -> {response.json()[0]['name']}"
+        )
+    elif response.elapsed.total_seconds() > 0.5:
+        response.failure(
+            f"Request took too long -> {response.elapsed.total_seconds()}"
+        )
+
+
