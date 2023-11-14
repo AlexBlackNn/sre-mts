817a017296a822cb8e99dd75d68533b3cb386fc1
alexblacknn <alexblacknn@yandex.ru>
2023-11-14 11:36:49 +0300

    create service function writing test data

--- HT/locust/src/repository/postgres.py
+++ HT/locust/src/repository/postgres.py
@@ -18,39 +18,49 @@ class PostgresRepo:
             args = ','.join(
                 cursor.mogrify(number_params, item).decode() for item in data
             )
-            cursor.execute(f"""INSERT INTO {schema} VALUES {args} RETURNING id; """)
+            cursor.execute(
+                f"""INSERT INTO {schema} VALUES {args} RETURNING id; """
+            )
             return cursor.fetchall()
 
 
 if __name__ == '__main__':
-    postgres_repo = PostgresRepo(config.DSN)
 
-    data = [
+    city_data = [
         ('TestCity1',),
         ('TestCity2',),
-        ('TestCity3', ),
-        ('TestCity4', ),
-        ('TestCity5', ),
-        ('TestCity6', ),
-        ('TestCity7', ),
-        ('TestCity8', )
+        ('TestCity3',),
+        ('TestCity4',),
+        ('TestCity5',),
+        ('TestCity6',),
+        ('TestCity7',),
+        ('TestCity8',)
     ]
-    schema = 'cities (name)'
-    ids = postgres_repo.write(data, schema)
-    print(ids)
-    data = [
+
+    forecast_data = [
         (1, 0, 30, "Sunny day"),
         (2, 0, 25, "Wet day"),
         (3, 0, 30, "Sunny day"),
         (4, 0, 25, "Wet day"),
     ]
-    schema = 'forecast ("cityId","dateTime",temperature,summary)'
-    ids = postgres_repo.write(data, schema)
-    print(ids)
-    with PostgresConnectoion(config.DSN) as postgresql_connection:
-        cursor = postgresql_connection.cursor()
-        sql = '''SELECT * FROM cities'''
-        cursor.execute(sql)
-        postgresql_connection.commit()
-        publisher_records = cursor.fetchall()
-        print(publisher_records)
+
+    def write_test_data(city_data, forecast_data):
+        postgres_repo = PostgresRepo(config.DSN)
+
+        schema = 'cities (name)'
+        ids = postgres_repo.write(city_data, schema)
+        print(ids)
+
+        schema = 'forecast ("cityId","dateTime",temperature,summary)'
+        ids = postgres_repo.write(forecast_data, schema)
+        print(ids)
+        with PostgresConnectoion(config.DSN) as postgresql_connection:
+            cursor = postgresql_connection.cursor()
+            sql = '''SELECT * FROM cities'''
+            cursor.execute(sql)
+            postgresql_connection.commit()
+            publisher_records = cursor.fetchall()
+            print(publisher_records)
+
+
+    write_test_data(city_data, forecast_data)
