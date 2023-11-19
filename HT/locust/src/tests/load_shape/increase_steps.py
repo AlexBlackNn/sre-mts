d93b8baada638f3eead42485e8eb9b291b10951f
alex_black <alexblacknn@yandex.ru>
2023-11-19 18:23:35 +0300

    some info was added to HT doc, related to work

--- HT/locust/src/tests/load_shape/increase_steps.py
+++ HT/locust/src/tests/load_shape/increase_steps.py
@@ -2,9 +2,9 @@ from locust import  LoadTestShape
 
 class StagesShape(LoadTestShape):
     stages = [
-        {"duration": 10, "users": 1, "spawn_rate": 1},
-        {"duration": 20, "users": 2, "spawn_rate": 1},
-        {"duration": 30, "users": 3, "spawn_rate": 1},
+        {"duration": 20, "users": 1, "spawn_rate": 1},
+        {"duration": 40, "users": 2, "spawn_rate": 1},
+        {"duration": 60, "users": 3, "spawn_rate": 1},
     ]
 
     def tick(self):
