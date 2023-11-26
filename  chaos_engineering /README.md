d5e5a097d997851e99f09974b7014ed57eb6d2cd
alex_black <alexblacknn@yandex.ru>
2023-11-26 13:01:25 +0300

    add docs with chaos engineering tests and stable load

---
+++  chaos_engineering /README.md
@@ -0,0 +1,2 @@
+sudo ./blade create network loss --interface lo  --remote-port 8081 --percent 100  --timeout 40
+ 
---
+++ HT/locust/src/tests/load_shape/stable_min_load.py
@@ -0,0 +1,13 @@
+from locust import LoadTestShape
+
+
+class StagesShape(LoadTestShape):
+    stages = [
+        {"duration": 600, "users": 1, "spawn_rate": 1},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                return stage["users"], stage["spawn_rate"]
